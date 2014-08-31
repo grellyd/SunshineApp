9c0a7579e54a52b4b9325445a211ceac5c9837a3
Legendary Lellow <graham.brown137@gmail.com>
2014-08-31 12:26:03 -0700

    Share Action provider is working correctly

--- app/src/main/java/com/example/graham/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/graham/sunshine/app/DetailActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +17,13 @@ import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
 
-    ShareActionProvider mShareActionProvider;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_activity);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -33,11 +32,11 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail_activity, menu);
-        MenuItem shareItem = menu.findItem(R.id.action_share);
-        mShareActionProvider = new ShareActionProvider(getApplicationContext());
-        ShareActionProvider newShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
-        mShareActionProvider = newShareActionProvider;
+        getMenuInflater().inflate(R.menu.detail, menu);
+//        MenuItem shareItem = menu.findItem(R.id.action_share);
+//        mShareActionProvider = new ShareActionProvider(getApplicationContext());
+//        ShareActionProvider newShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+//        mShareActionProvider = newShareActionProvider;
         return true;
     }
 
@@ -52,40 +51,66 @@ public class DetailActivity extends ActionBarActivity {
             startActivity(launchSettings);
             return true;
         }
-        if (id == R.id.action_share){
-            Intent launchShare = new Intent(Intent.ACTION_SEND);
-            String shareString = "";
-            if (getIntent().hasExtra(Intent.EXTRA_TEXT)){
-                shareString = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-                shareString = shareString + " #Sunshine";
-                launchShare.putExtra(Intent.EXTRA_TEXT, shareString);
-                mShareActionProvider.setShareIntent(launchShare);
-                Log.v("Share", shareString);
-            } else Log.v("Share", "No Extras");
-
-        }
+//        if (id == R.id.action_share){
+//            Intent launchShare = new Intent(Intent.ACTION_SEND);
+//            String shareString = "";
+//            if (getIntent().hasExtra(Intent.EXTRA_TEXT)){
+//                shareString = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+//                shareString = shareString + " #Sunshine";
+//                launchShare.putExtra(Intent.EXTRA_TEXT, shareString);
+//                if (mShareActionProvider != null) {
+//                    mShareActionProvider.setShareIntent(launchShare);
+//                }else Log.d(LOG_TAG);
+//            } else Log.v("Share", "No Extras");
+//
+//        }
         return super.onOptionsItemSelected(item);
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASHTAG = " #Sunshine";
 
-        public PlaceholderFragment() {
+        private String mForecastStr;
+        private ShareActionProvider mShareActionProvider;
+
+        public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail_activity, container, false);
+
             Intent intent = getActivity().getIntent();
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
+                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
                 TextView contentView = (TextView) rootView.findViewById(R.id.content_textview);
-                contentView.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+                contentView.setText(mForecastStr);
             }
-
             return rootView;
+        }
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.detailfragment, menu);
+            MenuItem shareItem = menu.findItem(R.id.action_share);
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else Log.d(LOG_TAG, "ShareActionProvider is Null?");
         }
     }
 }
