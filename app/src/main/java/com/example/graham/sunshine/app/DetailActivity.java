e694906c5bca65dee115b4759b1468db4d88d210
Legendary Lellow <graham.brown137@gmail.com>
2014-08-31 11:37:22 -0700

    Removal of superfulous detail.xml

--- app/src/main/java/com/example/graham/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/graham/sunshine/app/DetailActivity.java
@@ -14,9 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-//import android.widget.ShareActionProvider;
-
-
 public class DetailActivity extends ActionBarActivity {
 
     ShareActionProvider mShareActionProvider;
